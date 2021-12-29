9116a85a700643758db84a260e0e25a5249eaf55
Tino <ivanov.konstantin.ict@gmail.com>
2021-12-29 18:14:51 +0100

    Created Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import Greet from "./components/Greet";
-import Welcome from "./components/Welcome";
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Services from './pages/services';
+import Contact from './pages/contact';
+import SignUp from './pages/signup';
 
 function App() {
   return (
-    <div className="App">
-        <Greet/>
-        <Welcome/>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/services' component={Services} />
+        <Route path='/contact-us' component={Contact} />
+        <Route path='/sign-up' component={SignUp} />
+      </Routes>
+    </Router>
   );
 }
 
